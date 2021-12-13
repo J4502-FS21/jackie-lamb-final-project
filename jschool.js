6d289914bcec899fcac012c9840a6918ed4dd1de
Jackie Lamb <jacqueline.renee20@gmail.com>
2021-12-13 11:37:57 -0600

    adding java

---
+++ jschool.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var allInputElements = document.querySelectorAll('input');
+var submitButton = document.getElementById("submit");
+var answerElement = document.getElementById("answerTally");
+
+var numberOfCorrectAnswers;
+
+var eachInput;
+
+submitButton.addEventListener('click', function() {
+
+  numberOfCorrectAnswers = 0;
+
+  allInputElements.forEach((item, i) => {
+    if (item.type === 'radio' && item.checked && item.classList.contains('correct')) {
+      numberOfCorrectAnswers = numberOfCorrectAnswers + 1;
+    }
+  });
+
+
+  answerElement.innerHTML = "You got <span>" + numberOfCorrectAnswers + "</span> correct!";
+
+});
